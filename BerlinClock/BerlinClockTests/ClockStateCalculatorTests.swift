fe0b978cf75967c6cbe91d020f01ee06699a550c
2026-DEV2-034 <2026-DEV2-034@users.noreply.github.com>
2026-02-08 12:30:49 +0100

    test: add test file for clock state calculator (red) - renamed BerlinClockTests.swift to ClockStateCalculatorTests.swift - declared sut of type ClockStateCalculator

---
+++ BerlinClock/BerlinClockTests/ClockStateCalculatorTests.swift
@@ -0,0 +1,18 @@
+//
+//  ClockStateCalculatorTests.swift
+//  ClockStateCalculatorTests
+//
+//  Created by 2026-DEV2-034 on 08/02/2026.
+//
+
+import Testing
+@testable import BerlinClock
+
+struct ClockStateCalculatorTests {
+
+    var sut: ClockStateCalculator!
+
+    init() {
+        sut = ClockStateCalculator()
+    }
+}

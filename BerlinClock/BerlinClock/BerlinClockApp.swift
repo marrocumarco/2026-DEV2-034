4a24130ac3ec2080cffef27b0233c608a34fe6a3
2026-DEV2-034 <2026-DEV2-034@users.noreply.github.com>
2026-02-08 12:07:35 +0100

    chore: initialize iOS project

---
+++ BerlinClock/BerlinClock/BerlinClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  BerlinClockApp.swift
+//  BerlinClock
+//
+//  Created by 2026-DEV2-034 on 08/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct BerlinClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BerlinClock/BerlinClock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BerlinClock
+//
+//  Created by 2026-DEV2-034 on 08/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

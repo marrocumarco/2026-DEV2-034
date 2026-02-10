1124b66726d2143802ef7cda74bad44791e69ff7
2026-DEV2-034 <2026-DEV2-034@users.noreply.github.com>
2026-02-10 22:06:39 +0100

    test: startClock method calls getClockState of the use case (red) - add mock implementation of getClockState to ClockUseCaseMock

--- BerlinClock/BerlinClockTests/ClockViewModelTests.swift
+++ BerlinClock/BerlinClockTests/ClockViewModelTests.swift
@@ -4,13 +4,18 @@
 //
 //  Created by 2026-DEV2-034 on 10/02/2026
 //
-	
 
 import Foundation
+import SwiftUI
+import Testing
+
 @testable import BerlinClock
 
 class ClockUseCaseMock: ClockUseCaseProtocol {
-
+    var getClockStateCalled = false
+    func getClockState() -> AsyncStream<PresentationClockState> {
+        getClockStateCalled = true
+    }
 }
 
 struct ClockViewModelTests {
@@ -20,5 +25,12 @@ struct ClockViewModelTests {
     init() {
         useCaseMock = ClockUseCaseMock()
         sut = ClockViewModel(clockUseCase: useCaseMock)
+    }
+
+    @Test func `startClock calls the method getClockState of the use case`() {
+
+        sut.startClock()
+
+        #expect(useCaseMock.getClockStateCalled)
     }
 }

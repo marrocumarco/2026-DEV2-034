a6a476848efcdab384e47e1aa77ee79d46ad50c9
2026-DEV2-034 <2026-DEV2-034@users.noreply.github.com>
2026-02-10 18:39:24 +0100

    feat: create clock use case (green) - create ClockStateCalculatorProtocol - create TimeProviderProtocol

--- BerlinClock/BerlinClock/ClockUseCase.swift
+++ BerlinClock/BerlinClock/ClockUseCase.swift
@@ -4,6 +4,7 @@
 //
 //  Created by 2026-DEV2-034 on 10/02/2026
 //
-	
 
 import Foundation
+
+struct ClockUseCase {}
--- BerlinClock/BerlinClock/TimeProvider.swift
+++ BerlinClock/BerlinClock/TimeProvider.swift
@@ -4,6 +4,9 @@
 //
 //  Created by 2026-DEV2-034 on 10/02/2026
 //
-	
 
 import Foundation
+
+protocol TimeProviderProtocol {
+
+}

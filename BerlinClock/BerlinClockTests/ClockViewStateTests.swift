2fa8ae081fd98438a638eb9d802761401f02ae91
2026-DEV2-034 <2026-DEV2-034@users.noreply.github.com>
2026-02-11 19:10:59 +0100

    test: check formatted time is correct (red)

--- BerlinClock/BerlinClockTests/ClockViewStateTests.swift
+++ BerlinClock/BerlinClockTests/ClockViewStateTests.swift
@@ -14,6 +14,18 @@ import SwiftUI
 
 struct ClockViewStateTests {
 
+    @Test(arguments: ["00:00:00", "23:59:59"])
+    func `the timestamp corresponds to the formatted time of the state`(expectedTimeString: String) {
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm:ss"
+        let time = dateFormatter.date(from: expectedTimeString)!
+
+        let sut = ClockViewState(from: PresentationClockState(time: time, state: ClockState(secondsLamp: .yellow, fiveHoursRow: [], singleHoursRow: [], fiveMinutesRow: [], singleMinutesRow: [])))
+
+        #expect(expectedTimeString == sut.time)
+    }
+
     @Test func `seconds lamp color is yellow when on`() throws {
 
         let sut = ClockViewState(from: PresentationClockState(time: Date(timeIntervalSince1970: 0), state: ClockState(secondsLamp: .yellow, fiveHoursRow: [], singleHoursRow: [], fiveMinutesRow: [], singleMinutesRow: [])))
